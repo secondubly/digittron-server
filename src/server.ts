693a9defa4c0c9cb9239e84ef6d69f76cb81e315
secondubly <secondubly@gmail.com>
2023-10-22 10:47:47 -0400

    twitch auth handling

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,32 @@
 import Fastify, { FastifyInstance, FastifyError } from 'fastify'
 
+
+interface TwitchAuthRequest {
+    code: string,
+    scope: string,
+    state: string
+}
+
 const server: FastifyInstance = Fastify()
+
+server.get('/', async(req, res) => {
+    return 'hello world\n'
+})
 
 server.get('/ping', async(_req, _res) => {
     return 'pong\n'
 })
 
-server.listen(8080, (err, address) => {
-    if (err) {
+server.get<{
+    Querystring: TwitchAuthRequest
+}>('/digittron', async(req, res) => {
+    const { code } = req.query
+    console.log(req)
+})
+
+server.listen({ port: 3000 })
+    .then((address) => console.log(`Server listening on ${address}`))
+    .catch((err: FastifyError) => {
         console.error(err)
         process.exit(1)
-    }
-    console.log(`Server listening at ${address}`)
-})
+    })
