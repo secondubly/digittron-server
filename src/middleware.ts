b22bac21b698ea0325d14414e48bc0e9e82b3c90
secondubly <secondubly@gmail.com>
2023-11-03 13:20:48 -0400

    more boilerplate work

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,4 @@
 import { defineMiddleware } from "astro:middleware";
-import crypto from 'crypto'
 export const onRequest = defineMiddleware((context, next) => {
     if(context.url.pathname === '/') {
         // setup session data
