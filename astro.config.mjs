de5526e4adf83db056a8d340f6bdc4dfea000853
secondubly <secondubly@gmail.com>
2023-11-03 12:53:46 -0400

    initial session data work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,4 @@
 import { defineConfig } from 'astro/config';
-import { loadEnv } from 'vite'
-const { TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
-import node from "@astrojs/node"
-import auth from 'auth-astro'
-import TwitchProvider from "@auth/core/providers/twitch"
-import { env } from 'process';
-
-const twitch = TwitchProvider({
-    clientId: TWITCH_CLIENT_ID,
-    clientSecret: TWITCH_CLIENT_SECRET
-})
 
 // https://astro.build/config
-export default defineConfig({
-    output: 'server',
-    integrations: [
-        auth({
-            providers: [
-                twitch
-            ]
-        })
-    ],
-    adapter: node({
-        mode: 'standalone'
-    })
-});
+export default defineConfig({});
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,9 @@
-export async function onRequest({ locals, request }, next) {
-    locals.title = "Hello"
-    return next()
-}
+import { defineMiddleware } from "astro:middleware";
+import crypto from 'crypto'
+export const onRequest = defineMiddleware((context, next) => {
+    if(context.url.pathname === '/') {
+        // setup session data
+    }
+
+    next()
+});
