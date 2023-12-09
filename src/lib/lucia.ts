c6e59409437a3d7123681c1cb9b1d3cbd5c6e723
secondubly <secondubly@gmail.com>
2023-12-09 12:53:47 -0500

    set session expiration date

--- src/lib/lucia.ts
+++ src/lib/lucia.ts
@@ -4,6 +4,7 @@ import { prisma as adapter } from "@lucia-auth/adapter-prisma"
 import { createContext } from "@secondubly/digittron-db"
 export const { prisma } = await createContext()
 
+const ONE_DAY = 86400000; // 24 hours in milliseconds
 export const auth = lucia({
     env: "DEV",
     middleware: astro(),
@@ -16,6 +17,10 @@ export const auth = lucia({
         return {
             username: data.username
         }
+    },
+    sessionExpiresIn: {
+        activePeriod: ONE_DAY,
+        idlePeriod: ONE_DAY * 3 // three days
     }
 });
 
