0096274a056e0a7be890793ec190f357f394251f
secondubly <secondubly@gmail.com>
2023-12-25 11:04:02 -0500

    working on state toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
-import tailwind from "@astrojs/tailwind";
-
-import preload from "astro-preload";
+import { defineConfig } from 'astro/config'
+import node from "@astrojs/node"
+import tailwind from "@astrojs/tailwind"
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +11,5 @@ export default defineConfig({
   adapter: node({
     mode: "standalone"
   }),
-  integrations: [tailwind(), preload()]
+  integrations: [tailwind(), vue()]
 });
