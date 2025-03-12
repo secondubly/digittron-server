9154441dc91a53c00f68c9d194d16be442316263
tai m. <secondubly@gmail.com>
2025-03-11 21:43:23 -0700

    remove compiled js files

--- frontend/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HomePage = void 0;
-const HomePage = () => {
-    return (<>
-			<div>Welcome!</div>
-		</>);
-};
-exports.HomePage = HomePage;
